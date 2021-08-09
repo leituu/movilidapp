ea0b4f6d260faa8d1992a6082b755363ed005402
leoturzi <leogturzi@gmail.com>
2021-08-08 22:39:58 -0300

    Activate google Sheets

--- movilidapp.js
+++ movilidapp.js
@@ -1,13 +1,13 @@
 
-// const scriptURL = 'https://script.google.com/macros/s/AKfycbxs1YgF1N54FRD2eJ2y0_7jhkhEqLwgi1FLMHRFUARWhcXXGYp2CFZQZqijOyYTUJqN/exec'
-// const form = document.forms['submit-form']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxs1YgF1N54FRD2eJ2y0_7jhkhEqLwgi1FLMHRFUARWhcXXGYp2CFZQZqijOyYTUJqN/exec'
+const form = document.forms['submit-form']
 
-// form.addEventListener('submit', e => {
-// e.preventDefault()
-// fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-//     .then(response => alert('Movimiento Cargado!', response))
-//     .catch(error => alert.error('Error!', error.message))
-// })
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert('Movimiento Cargado!', response))
+    .catch(error => alert.error('Error!', error.message))
+})
 
 const sbmt = document.querySelector('#enviar');
 sbmt.addEventListener('click', () => {
