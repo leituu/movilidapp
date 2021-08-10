09e26e827a57deb5b64ce051021af4acc7ec8ca5
leoturzi <leogturzi@gmail.com>
2021-08-10 09:35:34 -0300

    Activate google sheets

--- movilidapp.js
+++ movilidapp.js
@@ -3,14 +3,10 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxs1YgF1N54FRD2eJ2y0_
 const form = document.forms['submit-form']
 
 form.addEventListener('submit', e => {
-e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert('Movimiento Cargado!', response))
-    .catch(error => alert.error('Error!', error.message))
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => alert('Movimiento Cargado!', response))
+        .catch(error => alert.error('Error!', error.message));
+        document.getElementById('form').reset()
 })
 
-const sbmt = document.querySelector('#enviar');
-sbmt.addEventListener('click', () => {
-    const form = document.getElementsByName('submit-form');
-form.reset();
-})
